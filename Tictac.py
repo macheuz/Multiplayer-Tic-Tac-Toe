19e55484c642d550845c458526a47418fa035e0d
Matheus <machadosmatheus.0@gmail.com>
2022-04-27 08:27:20 -0300

    Third commit

--- Tictac.py
+++ Tictac.py
@@ -1,4 +1,5 @@
 import numpy as array
+from sqlalchemy import false
 
 
 def createTable():
@@ -63,6 +64,25 @@ def verifyTableDiagonal(table):
         print("Player O wins")
         return False
     return True
+
+def verifyTableDiagonal1(table):
+    contX = 0
+    contO = 0
+    for x in range (0,3):
+        for i in range(2,-1, -1):
+            if table[x][i]== '[x]':
+                contX = contX+1
+            if table [x][i] == '[o]':
+                contO = contO+1
+    if contX == 3:
+        print()
+        print("Player X wins")
+        return False
+    elif contO == 3:
+        print()
+        print("Player O wins")
+        return False
+    return True
     
 def playWithX(table):
     line = int(input("Put the line you want to fill: "))-1
@@ -91,13 +111,13 @@ table = createTable()
 print(table[0][0])
 print(table)
 x=0
-while(x<=9 and (verifyTablecoluns(table) == True and verifyTableDiagonal(table)== True and verifyTablelines(table)== True)):
+while(x<=9 and (verifyTablecoluns(table) == True and verifyTableDiagonal(table)== True and verifyTableDiagonal1(table)== True and verifyTablelines(table)== True)):
     print("Player X:")
     print()
     playWithX(table)
     print(table)
     x=x+1
-    if x == 9 or (verifyTablecoluns(table) == False or verifyTableDiagonal(table)== False or verifyTablelines(table)== False):
+    if x == 9 or (verifyTablecoluns(table) == False or verifyTableDiagonal(table)== False or verifyTableDiagonal1(table)== False or verifyTablelines(table)== False):
         break
     print('---'*30)
     print("Player O")
